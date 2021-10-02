ce68677667ceb2b1c935e0bc7ceedd4b1bfd753d
juanriverarq <45473697+juanriverarq@users.noreply.github.com>
2021-10-02 18:52:55 -0500

    app

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Pelicula } from '../interfaces/interfaces';
+import { MoviesService } from '../services/movies.service';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page implements OnInit{
+
+  peliculasNuevas: Pelicula[] = [];
+  
+  slideOpts = {
+    slidesPerView: 1.3,
+    freeMode: true
+  };
+
+  constructor( private movieService: MoviesService ) {}
+
+  ngOnInit(){
+    this.movieService.getFeature()
+    .subscribe(resp => {
+      this.peliculasNuevas = resp.results;
+    });
+  }
+
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  url: 'https://api.themoviedb.org/3',
+  apiKey: 'aefc7125742296e8015a42320c1719a5',
+  imgPath: 'https://image.tmdb.org/t/p'
+};
